ab2d9a86640f7eb2691198c47b366df233db61e2
Lior Levy <liorlevy@cs.washington.edu>
2018-04-07 13:07:23 -0700

    maybe working:

--- js/ColorTag.js
+++ js/ColorTag.js
@@ -1,13 +1,13 @@
 (function() {
 
     'use strict';
-var json;
+    var json = "";
     window.onload = function() {
+      alert("hi");
       console.log("outside"+json);
     };
 
-
-    function getColors(var url) {
+    function getColors(url) {
         fetch("https://apicloud-colortag.p.mashape.com/tag-url.json?sort=weight&url="+url).then(function(response){
           json = response;
           console.log(json);
