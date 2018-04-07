c0b27a877f7a5e0b6f3296fb9b94d7b306c0ba25
Lior Levy <liorlevy@cs.washington.edu>
2018-04-07 15:51:19 -0700

    color tag

--- js/ColorTag.js
+++ js/ColorTag.js
@@ -2,7 +2,7 @@
     'use strict';
     var json = "";
     window.onload = function() {
-      getColors("https://imgur.com/OxCuqhw");
+      getColors("https://i.imgur.com/OxCuqhw.jpg");
       console.log("outside"+json);
     };
 
@@ -30,19 +30,27 @@
 
     function getColors(url){
       var proxyUrl = 'https://mighty-beach-27822.herokuapp.com/',
-        targetUrl = 'http://mkweb.bcgsc.ca/color-summarizer/?url='+url+'&precision=vhigh&json=1'
+        targetUrl = 'http://mkweb.bcgsc.ca/color-summarizer/?url='+url+'&precision=medium&json=1'
         fetch(proxyUrl + targetUrl)
           .then(handleResponse)
           .then(function(json){
-            console.log(json);
+            var jsonArr = json["clusters"];
+            var hexArr = [];
 
-          // .catch(e => {
-          //   console.log(e);
-          //   return e;
-      });
+           jsonArr.forEach(function(cluster) {
+              hexArr.push(cluster["hex"][0]);
+            });
+
+            // for(let i=0; i<jsonArr.length; i++){
+            //   hexArr.push(jsonArr[i]["hex"][0]);
+            //
+            // }
+            console.log(hexArr);
+          });
     }
 
     function handleResponse(response) {
+      console.log(response);
         if (response.ok) {
             return response.json();
         } else {
