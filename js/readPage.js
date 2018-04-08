f73c0c51c109699450ccb23d85cd2346de519fae
Nicole Riley <nriley16@uw.edu>
2018-04-07 17:05:22 -0700

    changes to readPage

--- js/readPage.js
+++ js/readPage.js
@@ -6,7 +6,22 @@ var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 let request = "http://api.screenshotlayer.com/api/capture?access_key=ddccf18819be3976a799d69c564d1401&url=" + web + "&format=JPG";
 
 fetch(proxyUrl + request)
+	.then(handleResponse)
 	.then(function(response) {
+		console.log(response);
+		console.log(response.json());
 		console.log(response.type);
 		return response;
 	});
+
+	function handleResponse(response) {
+		console.log(response);
+			if (response.ok) {
+					return response.json();
+			} else {
+					return response.json()
+							.then(function (err) {
+									throw new Error(err.errorMessage);
+							});
+			}
+	}
