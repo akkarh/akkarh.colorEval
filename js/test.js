d89ab7489816c9ce6d1a51cae24fa7dbbaa3d1bf
saihn <saihn@uw.edu>
2018-04-07 18:36:26 -0700

    latest

--- js/test.js
+++ js/test.js
@@ -1,3 +1,17 @@
+
+
+(function () {
+
+window.onload = function() {
+
+	alert("gvhvgh");
+};
+
+
+
+// 
+// function postImage() {
+// alert("gvhvgh");
 // var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 // 
 // let imageURL = "https://static-secure.guim.co.uk/sys-images/Guardian/Pix/pictures/2008/04/17/dolphin11a.jpg";
@@ -9,9 +23,7 @@
 // });
 // 
 // var myInit = {method: 'POST', headers: headers, cache: 'default'};
-// 
-// 
-// function postImage() {
+// 	
 // 	fetch(proxyUrl + request, myInit)
 // 		.then(function(response) {
 // 			console.log(response.json);
@@ -19,20 +31,8 @@
 // 		});
 // }
 
-$.ajax({
-	url: 'https://api.imgur.com/3/image',
-	type: 'post',
-	headers: {
-		Authorization: 'Client-ID 0d7270ef3e5e7e3'
-	},
-	data: {
-		image: 'https://static-secure.guim.co.uk/sys-images/Guardian/Pix/pictures/2008/04/17/dolphin11a.jpg'
-	},
-	dataType: 'json',
-	success: function(response) {
-		if(response.success) {
-			console.log(response.data.link);
-		}
-	}
-});
+
+
+})();
+
 
